7b8ebc08a9148d42f6ea5d740324c638bb29f078
Ian Kachan <kachan.yanf@gmail.com>
2021-11-23 01:43:06 +0500

    Initial commit

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,7 +1,6 @@
 package koschei.config;
 
-import koschei.models.Island2;
-import koschei.models.Wood3;
+import koschei.models.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +9,13 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
 
-    @Bean
+    /*@Bean
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
+
+    @Bean
+    public static Egg6 getEgg(Needle7 needle) {
+        return new Egg6(needle);
+    }*/
 }
